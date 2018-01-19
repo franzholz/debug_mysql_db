f5a9b69a8e64a1f2a2b6be2087048db8d4997d9d
Franz Holzinger <holzinger@ttproducts.de>
2018-01-19 13:31:55 +0100

    next version 0.6.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = array(
     'description' => 'Extends \\TYPO3\\CMS\\Core\\Database\\DatabaseConnection (former t3lib_db) to show Errors and Debug-Messages. Useful for viewing and debugging of sql-queries. Shows error messages if they occur. (For TYPO3 before 6.1 see versions 0.5.x)',
     'category' => 'misc',
     'shy' => 0,
-    'version' => '0.6.2',
+    'version' => '0.6.3',
     'dependencies' => '',
     'conflicts' => 'dbal',
     'priority' => '',
@@ -32,9 +32,6 @@ $EM_CONF[$_EXTKEY] = array(
         ),
         'suggests' => array(
         ),
-    ),
-    '_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"dae2";s:16:"ext_autoload.php";s:4:"55c0";s:21:"ext_conf_template.txt";s:4:"b1f6";s:12:"ext_icon.gif";s:4:"8ea6";s:17:"ext_localconf.php";s:4:"c1c4";s:39:"Classes/Database/DatabaseConnection.php";s:4:"8887";s:14:"doc/manual.sxw";s:4:"70df";}',
-    'suggests' => array(
-    ),
+    )
 );
 
