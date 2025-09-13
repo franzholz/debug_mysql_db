289812ccdbe2231f0f2f3a2a7487ec48db2ea3d7
Franz Holzinger <franz.holzinger@festwein.de>
2025-09-13 18:39:01 +0200

    -

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Debug Mysql or other DB',
-    'description' => 'Extends \\TYPO3\\CMS\\Core\\Database\\DatabaseConnection and \\TYPO3\\CMS\\Core\\Database\\Connection to show Errors and Debug-Messages. Debugging of sql-queries by debug and FileWriter.',
+    'description' => 'Extends \\TYPO3\\CMS\\Core\\Database\\Connection and \\TYPO3\\CMS\\Typo3DbLegacy\\Database\\DatabaseConnection to show Errors and Debug-Messages. Debugging of sql-queries by debug and FileWriter.',
     'category' => 'misc',
     'version' => '1.9.4',
     'state' => 'stable',
@@ -12,11 +12,11 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '13.0.0-13.4.99',
-            'php' => '8.1.0-8.4.99',
+            'php' => '8.2.0-8.4.99',
         ],
         'suggests' => [
             'typo3db_legacy' => '1.2.0-1.2.99',
-            'fh_debug' => '0.17.0-0.20.99',
+            'fh_debug' => '0.18.0-0.20.99',
         ],
     ]
 ];
