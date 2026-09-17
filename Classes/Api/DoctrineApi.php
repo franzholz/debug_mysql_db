3d55e1a70031bb51a42085f2024807afa61996fd
Franz Holzinger <franz.holzinger@festwein.de>
2026-09-17 14:57:17 +0200

    fix getExpandedQuery PHP errors

--- Classes/Api/DoctrineApi.php
+++ Classes/Api/DoctrineApi.php
@@ -21,72 +21,84 @@ use TYPO3\CMS\Core\Database\Connection;
 
 class DoctrineApi implements SingletonInterface {
 
-    // mit QueryBuilder Methode ergänzen
     public function getExpandedQuery ($query, $params, $types)
     {
         $questionmarkMode =
-            strpos((string) $query, '(?') ||
-            strpos((string) $query, '= ?');
+        strpos((string) $query, '(?') ||
+        strpos((string) $query, '= ?') ||
+        strpos((string) $query, '=?');
+
         $parts = [];
-        $partsIndex = 0;
         if ($questionmarkMode) {
             $parts = explode('?', (string) $query);
         }
         $expandedQuery = $query;
+        $preparedValues = [];
 
         foreach ($params as $paramName => $value) {
-            if (
-                is_array($types) &&
-                isset($types[$paramName])
-            ) {
+            // 1. Sichere Typ-Ermittlung
+            if (is_array($types) && isset($types[$paramName])) {
                 $type = $types[$paramName];
             } else if (is_int($value)) {
                 $type = Connection::PARAM_INT;
-            } else if (is_string($value)) {
-                $type = Connection::PARAM_STR;
+            } else if (is_array($value)) {
+                $type = Connection::PARAM_STR_ARRAY; // Falls Array übergeben wird, nimm an es ist ein String-Array
             } else {
-                $type = Connection::PARAM_STR_ARRAY;
+                $type = Connection::PARAM_STR; // Standard für Skalare ist String
             }
 
+            // 2. Werte-Formatierung basierend auf Typ
             switch ($type) {
                 case Connection::PARAM_INT_ARRAY:
                     if (is_array($value)) {
-                        $value = implode(',', $value);
+                        $value = implode(',', array_map('intval', $value));
                     } else {
-                        continue 2;
+                        $value = intval($value);
                     }
                     break;
+
                 case Connection::PARAM_STR_ARRAY:
                     if (is_array($value)) {
                         $newValueArray = [];
                         foreach ($value as $subValue) {
-                            $newValueArray[] = '\'' . $subValue . '\'';
+                            $newValueArray[] = '\'' . addslashes((string)$subValue) . '\'';
                         }
                         $value = implode(',', $newValueArray);
                     } else {
-                        continue 2;
+                        $value = '\'' . addslashes((string)$value) . '\'';
                     }
                     break;
+
                 case Connection::PARAM_INT:
                     $value = intval($value);
                     break;
+
                 case Connection::PARAM_STR:
+                default:
                     if (is_array($value)) {
                         $value = current($value);
                     }
-                    $value = '\'' . (string) $value . '\'';
+                    $value = '\'' . addslashes((string)$value) . '\'';
                     break;
             }
 
+            // 3. Zuweisung je nach Platzhalter-Modus
             if ($questionmarkMode) {
-                $parts[$partsIndex++] = $value;
+                $preparedValues[] = (string)$value;
             } else {
-                $expandedQuery = str_replace(':' . $paramName, $value, (string) $expandedQuery);
+                $expandedQuery = str_replace(':' . $paramName, (string)$value, (string) $expandedQuery);
             }
         }
 
-        if ($questionmarkMode) {
-            $expandedQuery = implode('', $parts);
+        // 4. Reißverschluss-Verfahren für Fragezeichen-Queries
+        if ($questionmarkMode && !empty($parts)) {
+            $expandedQuery = '';
+            foreach ($parts as $index => $part) {
+                $expandedQuery .= $part;
+                if (isset($preparedValues[$index])) {
+                    $expandedQuery .= $preparedValues[$index];
+                }
+            }
         }
 
         return $expandedQuery;
